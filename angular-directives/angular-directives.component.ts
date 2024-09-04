9fa19472082f4361273e83e2bb57e883a3c1be6b
Ljojua15 <jojualasha15@gmail.com>
2024-09-04 10:34:34 +0400

    '3-lesson'

--- angular-directives/angular-directives.component.ts
+++ angular-directives/angular-directives.component.ts
@@ -2,17 +2,17 @@ import { Component, OnInit, signal } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { directives } from "./interface";
+import { PortfolioBuilderComponent } from "./portfolio-builder/portfolio-builder.component";
 @Component({
   selector: "app-angular-directives",
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, PortfolioBuilderComponent],
   templateUrl: "./angular-directives.component.html",
   styleUrl: "./angular-directives.component.scss",
 })
-export class AngularDirectivesComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error("Method not implemented.");
-  }
+export class AngularDirectivesComponent {
+  tab: string = "port";
+
   age: number = 0;
   isOlder: boolean = true;
 
@@ -21,10 +21,15 @@ export class AngularDirectivesComponent implements OnInit {
     { id: 2, name: "ngFor", usefull: true, color: "black" },
     { id: 3, name: "ngSwich", usefull: false, color: "yellow" },
     { id: 4, name: "ngClass", usefull: true, color: "green" },
-    { id: 5, name: "ngSlyle", usefull: true, color: "pink" },
+    { id: 5, name: "ngSlyle", usefull: false, color: "pink" },
     { id: 6, name: "ng-templete", usefull: true, color: "orange" },
     { id: 7, name: "ng-container", usefull: true, color: "brown" },
   ]);
+
+  changeTab(param: string) {
+    this.tab = param;
+    console.log("1");
+  }
 
   checkAge() {
     this.isOlder = this.age > 18;
